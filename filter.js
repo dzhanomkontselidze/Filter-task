89aacaf168d18fce26d81022734b8502a8bf6cc0
dzhanomkontselidze <dzhano.m.kontselidze@ukd.edu.ua>
2025-11-03 21:36:19 +0200

    Update filter.js
    repaired the code so it finally works

--- filter.js
+++ filter.js
@@ -1,36 +1,29 @@
-const toFilter = [3, 2, -9, null, '12','3a',[], undefined, '', 5]
+const toFilter = [3, 2, -9, null, '12', '3a', [], undefined, '', 5, NaN]
 
-function filtrovane(arr = [], 'number') {
-    console.log(arr)
-    const result  = []
-    if(!Array.isArray(arr)) {
+function filtrovane(arr = []) {
+    const result = []
+    if (!Array.isArray(arr)) {
         return result;
     }
 
-    for(let i = 0;i < arr.length;i++)
-    {
-        if(typeof arr[i] === "number" )
-        {
-            result.push(arr[i])
-        } 
-
-
-
-        if(typeof arr[i] === "string" && parseInt(arr[i], 10))
-        {
-            result.push(arr[i])
+    for (let i = 0; i < arr.length; i++) {
+        const item = arr[i];
+        if (typeof item === "number" && !isNaN(item)) {
+            result.push(item);
+        }
+        else if (typeof item === "string" && item.trim() !== '' && !isNaN(Number(item))) {
+            result.push(item);
         }
     }
-    
 
     return result;
 }
 
-console.log(parseInt("3a") == '3a')
-console.log(parseInt('30') == '30')
-console.log(filtrovane(toFilter))
-console.log("cірьога арсенал!!!!")
+
+console.log(`parseInt("3a") == '3a' -> ${parseInt("3a") == '3a'}`)
+console.log(`parseInt('30') == '30' -> ${parseInt('30') == '30'}`) 
 
 
-
+console.log('Результат фільтрації:', filtrovane(toFilter))
+console.log("7 прєдмєтав в мегаящікє!!!!")
 
